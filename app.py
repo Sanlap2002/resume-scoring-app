3248b9a8d743e6439980792c9e8e974a70f958c2
Sanlap Das <sanlapdas19@gmail.com>
2025-04-12 21:45:58 +0530

    added sentence-transformer

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer,util
 
-# Step 1: Extract resume text from PDF
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
 def extract_resume_text(uploaded_file):
     with open("temp_resume.pdf", "wb") as f:
         f.write(uploaded_file.read())
@@ -13,10 +16,16 @@ def extract_resume_text(uploaded_file):
 
 # Step 2: Vectorize and compute cosine similarity
 def get_similarity_score(resume_text, job_desc):
-    vectorizer = TfidfVectorizer(stop_words='english', max_features=1000)
-    tfidf_matrix = vectorizer.fit_transform([resume_text, job_desc])
-    score = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
-    return round(score * 100, 2)
+    #vectorizer = TfidfVectorizer(stop_words='english', max_features=1000)
+    #tfidf_matrix = vectorizer.fit_transform([resume_text, job_desc])
+    #score = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    embeddings=model.encode([resume_text, job_desc],convert_to_tensor=True)
+    cosine_score = util.cos_sim(embeddings[0], embeddings[1])
+    return round(float(cosine_score[0][0]) * 100, 2)
+
+    
+    
+
 
 # Streamlit UI
 st.title("🧠 Resume Scoring App")
@@ -26,14 +35,19 @@ uploaded_file = st.file_uploader("Upload Resume (PDF)", type=["pdf"])
 job_desc = st.text_area("Paste Job Description")
 
 if uploaded_file and job_desc:
-    with st.spinner("Analyzing..."):
-        resume_text = extract_resume_text(uploaded_file)
-        score = get_similarity_score(resume_text, job_desc)
-    st.success(f"✅ Resume Match Score: **{score}/100**")
+    if st.button("Get Score"):
+        with st.spinner("Analyzing..."):
+            resume_text = extract_resume_text(uploaded_file)
+            score = get_similarity_score(resume_text, job_desc)
 
-    if score > 75:
-        st.info("🟢 Strong match! Great job aligning your resume.")
-    elif score > 50:
-        st.warning("🟡 Decent match. You may want to tweak a few sections.")
-    else:
-        st.error("🔴 Low match. Consider tailoring your resume more to this job.")
+        st.success(f"✅ Resume Match Score: **{score}/100**")
+
+        if score > 75:
+            st.info("🟢 Strong match! Great job aligning your resume.")
+        elif score > 50:
+            st.warning("🟡 Decent match. You may want to tweak a few sections.")
+        else:
+            st.error("🔴 Low match. Consider tailoring your resume more to this job.")
+        
+else:
+    st.button("Get Score", disabled=True)
